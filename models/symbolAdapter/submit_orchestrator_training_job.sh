5931afbd546b4c1623ed2abd40efda34c7f2cb9e
magnieet <neeraj13788@gmail.com>
2025-07-13 00:00:08 +0530

    Auto-commit: 2025-07-13 00:00:01

--- models/symbolAdapter/submit_orchestrator_training_job.sh
+++ models/symbolAdapter/submit_orchestrator_training_job.sh
@@ -3,7 +3,7 @@
 
 # Configuration - Edit these values as needed
 model_type="salmonn"  # Options: "salmonn" or "qwen2"
-dataset_type="hvb-meld_emotion"  # Dataset type(s) to use
+dataset_type="voxceleb-voxpopuli"  # Dataset type(s) to use
 # dataset_type="voxceleb-voxpopuli"  # Dataset type(s) to use
 device="cuda:0"  # GPU device
 
@@ -123,13 +123,13 @@ echo "Log File: ${LOG_DIR}/${RUN_NAME}.log"
 echo "=========================================="
 
 # Submit job
-qsub -q longgpu.q -V -cwd \
+qsub -q gpu.q -V -cwd \
     $HOLD_FLAG \
     -l hostname=compute-0-9 \
     -l h_rt=72:00:00 \
     -o "${LOG_DIR}/${RUN_NAME}.log" \
     -j y \
-    -v CUDA_VISIBLE_DEVICES=1,\
+    -v CUDA_VISIBLE_DEVICES=2,\
 TODAY=${TODAY},\
 PYTHONUNBUFFERED=1,\
 RUN_NAME=${RUN_NAME},\
